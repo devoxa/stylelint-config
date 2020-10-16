64115cedeea7c5f6011e97747c3c7a94a0301ba2
queicherius <queicherius@gmail.com>
2020-10-16 04:05:59 +0000

    Add a more complex example

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: 'stylelint-config-standard-scss',
+  extends: [
+    // Extend the common stylistic conventions found within a handful of CSS styleguides
+    'stylelint-config-standard-scss',
+  ],
   rules: {
     // Change this rule because we want to be able to write code like the following:
     // .border-vertical-2 { border-top: 2px solid; border-bottom: 2px solid; }
@@ -19,5 +22,8 @@ module.exports = {
       'always',
       { except: ['first-nested'], ignoreComments: ['/prettier-ignore/'] },
     ],
+
+    // Change this rule so we always have consistent hex color lengths
+    'color-hex-length': 'long',
   },
 }
