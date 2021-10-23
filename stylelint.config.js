793cf8ddef5cb3768c1cc174e9409d1c99c80991
queicherius <queicherius@gmail.com>
2021-10-23 16:50:53 +0000

    Update config

--- stylelint.config.js
+++ stylelint.config.js
@@ -14,6 +14,16 @@ module.exports = {
     // Disable this rule because it clashes pretty hard with SCSS syntax (@for, @each, @return, @if, ...)
     'at-rule-empty-line-before': null,
 
+    // Disable this rule because it clashes with the Prettier formatting
+    'scss/operator-no-newline-after': null,
+
+    // Disable these rules because they can make properties harder to read than the longhand
+    'shorthand-property-no-redundant-values': null,
+    'declaration-block-no-redundant-longhand-properties': null,
+
+    // Disable this rule because we don't enforce any naming conventions for class selectors
+    'selector-class-pattern': null,
+
     // Change this rule to not clash with SCSS if/else statements
     'block-closing-brace-newline-after': ['always', { ignoreAtRules: ['if', 'else'] }],
 
@@ -28,5 +38,11 @@ module.exports = {
 
     // Change this rule so we always have consistent hex color lengths
     'color-hex-length': 'long',
+
+    // Change this rule so we always use the number notation for alpha values
+    'alpha-value-notation': 'number',
+
+    // Change this rule so we always use the legacy notation for color functions
+    'color-function-notation': 'legacy',
   },
 }
